09fb09ec2f8f7f2e09c749b26bb4e40a0980f249
alexey <alexey-melekhov1989@yandex.ru>
2021-11-28 15:30:55 +0500

    add task state machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,33 @@
 class Task < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :assignee, class_name: 'User', optional: true
+  belongs_to :author, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
 
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :author, presence: true
-    validates :description, length: { maximum: 500 }
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :author, presence: true
+  validates :description, length: { maximum: 500 }
+
+  state_machine :initial => :new_task do
+    event :to_development do
+      transition :new_task => :in_development
+    end
+    event :archive do
+      transition :new_task => :archived
+    end
+    event :to_development do
+      transition :new_task => :in_qa
+    end
+    event :to_qa do
+      transition :new_task => [:in_development, :in_code_review]
+    end
+    event :to_code_review do
+      transition :new_task => [:ready_for_release, :in_development]
+    end
+    event :ready_for_release do
+      transition :new_task => :released
+    end
+    event :to_released do
+      transition :new_task => :archived
+    end
+  end
 end
